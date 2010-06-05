ad14394011d24c50796d1c885c2174b98f975cd7
chris <chris@chris-laptop.(none)>
2010-06-05 11:51:33 +0100

    Added new email form to InvoiceBuilder web page aslong with divs for sections

--- war/WEB-INF/groovy/invoice/InvoiceGenerator.groovy
+++ war/WEB-INF/groovy/invoice/InvoiceGenerator.groovy
@@ -4,39 +4,79 @@ html.html() {
 	
 	body() {
 		p {h1("Invoice Generator") } 
-		form(name:"Generate", action:"InvoiceBuilder.groovy") {
-			table {
-				tr {
-					td "Invoice Number:" 
-					td{
-						input(type:"text", size:3, name:"number")
-					}  
+		
+		div(id:"pdfForm") {
+			form(name:"Generate", action:"InvoiceBuilder.groovy") {
+				table {
+					tr {
+						td "Invoice Number:" 
+						td{
+							input(type:"text", size:3, name:"number")
+						}  
+					}
+					tr {
+						td "Days:" 
+						td{
+							input(type:"text", size:3, name:"days")
+						}  
+					}
+					tr {
+						td "From Date:" 
+						td{
+							input(type:"text", size:12, name:"fromDate")
+						}  
+					}
+					tr {
+						td "To Date:" 
+						td{
+							input(type:"text", size:12, name:"body")
+						}  
+					}
+					tr { 
+						td{
+							input(type:"submit", name:"invoice-generator", value:"Generate")
+						}  
+					}
 				}
-				tr {
-					td "Days:" 
-					td{
-						input(type:"text", size:3, name:"days")
-					}  
-				}
-				tr {
-					td "From Date:" 
-					td{
-						input(type:"text", size:3, name:"fromDate")
-					}  
-				}
-				tr {
-					td "To Date:" 
-					td{
-						input(type:"text", size:3, name:"toDate")
-					}  
-				}
-				tr { 
-					td{
-						input(type:"submit", name:"submit")
-					}  
-				}
-				
 			}
 		}
+		
+		div(id:"emailForm") {
+			form(name:"Email", action:"EmailSender.groovy") {
+				table {
+					tr {
+						td "to" 
+						td{
+							input(type:"text", size:50, name:"toAddress", value:"melissa")
+						}  
+					}
+					tr {
+						td "from:" 
+						td{
+							input(type:"text", size:50, name:"fromAddress", value:"chris@cghsystems.net")
+						}  
+					}
+					tr {
+						td "Attatchment:" 
+						td{
+							input(type:"text", size:50, name:"attatchment")
+						}  
+					}
+					tr {
+						td "Body" 
+						td{
+							input(type:"text", size:100, name:"body", value:"Hi...")
+						}    
+					}
+					tr { 
+						td{
+							input(type:"submit", name:"email", value:"Send Email")
+						}  
+					}
+				}
+			}
+		}
+		
+		div(id:"emailSent") { p("Email Sent") }
 	}
 }
